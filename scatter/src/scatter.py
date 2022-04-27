185f5719475b832a62d1eb79d98038dc8a895098
Idonotwant <alps30318@gmail.com>
2022-04-27 13:14:54 +0800

    class form

--- scatter/src/scatter.py
+++ scatter/src/scatter.py
@@ -5,6 +5,7 @@ Description: scatter
 Brief_Log:
 2022/04/16 Start
 2022/04/18 v1 packed
+2022/04/27 class form
 Note: 
 python setting.py to make reference file
 python scatter.py to make scatter with log x axis(is usually used in frequence response)
@@ -19,78 +20,86 @@ from openpyxl.chart import (
     marker
 )
 
-class scatter():
-    def __init__(self,**kwgs) -> None:
-        self.titlek = kwgs['title']
-        self.titlev = None
-    def get(title):
-        pass
-
-bookpass = input('workbook position(absolute): ')
-bookpass = bookpass[1:-1]
-
-def find_col(col): #ex: "A"->1
-    col = col.upper()
-    col = "".join(reversed(col))
-    base = 1
-    colValue = 0
-    for i in range (len(col)):
-        colValue += base*(ord(col[i])-64)
-        base *= 26
-    return colValue
-
-def splitter(target): #ex: "BA123"->53,123
-    i = 0
-    while target[i].isalpha():
-        i+=1
-    return find_col(target[:i]) ,int(target[i:])
-
-
-
-# referrence
-title = 'B1'
-xlabel = 'B2'
-xend = 'B3'
-ylabel = 'B4'
-yend = 'B5'
-chartPos = 'B6'
-
-wb = load_workbook(bookpass)
-ws = wb.active
-
 blue = drawing.colors.ColorChoice(prstClr='blue')
 
-chart = ScatterChart(scatterStyle='marker')
-#scatterStyle=None,varyColors=NONE,dLbls=NONE,extLst=None
-chart.title = ws[ws[title].value].value
-chart.x_axis.title = ws[ws[xlabel].value].value
-chart.y_axis.title = ws[ws[ylabel].value].value
-chart.x_axis.scaling.logBase = 10
+class scatter():
+    title = 'B1'
+    xlabel = 'B2'
+    xend = 'B3'
+    ylabel = 'B4'
+    yend = 'B5'
+    chartPos = 'B6'
+    def __init__(self,**kwgs):
+        if 'name' in  kwgs.keys():
+            self.name = kwgs['name']
+        else:
+            self.name = 'newScatter'
+        if 'mode' in kwgs.keys():
+            self.mode = kwgs['mode']
+        else:
+            self.mode = 0
+        self.bookpass = self.get(self.mode)
+        self.read_plot()
+    def get(self,mode):
+        if not mode:
+            #input by typing
+            bookpass = input('worbook poisition(absolute: ')
+            #debug
+            bookpass = bookpass[1:-1]
+            return bookpass
+        else:#input by tkinter
+            pass
+    def read_plot(self):
+        wb = load_workbook(self.bookpass)
+        ws = wb.active
+        chart = ScatterChart(scatterStyle='marker')
+        #scatterStyle=None,varyColors=NONE,dLbls=NONE,extLst=None
+        chart.title = ws[ws[scatter.title].value].value
+        chart.x_axis.title = ws[ws[scatter.xlabel].value].value
+        chart.y_axis.title = ws[ws[scatter.ylabel].value].value
+        chart.x_axis.scaling.logBase = 10
 
-x_min_col,x_min_row = splitter(ws[xlabel].value)
-x_min_row+=1
-x_max_col,x_max_row = splitter(ws[xend].value)
+        x_min_col,x_min_row = self.splitter(ws[scatter.xlabel].value)
+        x_min_row+=1
+        x_max_col,x_max_row = self.splitter(ws[scatter.xend].value)
 
-y_min_col,y_min_row = splitter(ws[ylabel].value)
-y_min_row+=1
-y_max_col,y_max_row = splitter(ws[yend].value)
+        y_min_col,y_min_row = self.splitter(ws[scatter.ylabel].value)
+        y_min_row+=1
+        y_max_col,y_max_row = self.splitter(ws[scatter.yend].value)
 
-xvalues = Reference(ws, min_col=x_min_col, min_row=x_min_row, max_row=x_max_row)
-values = Reference(ws, min_col=y_min_col, min_row=y_min_row, max_row=y_max_row)
-series = Series(values, xvalues, title_from_data=False)
+        xvalues = Reference(ws, min_col=x_min_col, min_row=x_min_row, max_row=x_max_row)
+        values = Reference(ws, min_col=y_min_col, min_row=y_min_row, max_row=y_max_row)
+        series = Series(values, xvalues, title_from_data=False)
 
-chartPosV = ws[chartPos].value
+        chartPosV = ws[scatter.chartPos].value
 
-lineProp = drawing.line.LineProperties(solidFill = blue)
-series.graphicalProperties.line = lineProp
-chart.series.append(series)
+        lineProp = drawing.line.LineProperties(solidFill = blue)
+        series.graphicalProperties.line = lineProp
+        chart.series.append(series)
+
+        chart.style = 25
+        ws.add_chart(chart,chartPosV)
+
+        tmp = self.bookpass.split('.')
+        bookoutpass = tmp[0]+"_out."+tmp[1]
+        wb.save(bookoutpass)
+    def find_col(self,col): #ex: "A"->1
+        col = col.upper()
+        col = "".join(reversed(col))
+        base = 1
+        colValue = 0
+        for i in range (len(col)):
+            colValue += base*(ord(col[i])-64)
+            base *= 26
+        return colValue
+    def splitter(self,target): #ex: "BA123"->53,123
+        i = 0
+        while target[i].isalpha():
+            i+=1
+        return self.find_col(target[:i]) ,int(target[i:])
 
 
 
-chart.style = 25
-ws.add_chart(chart,chartPosV)
+if __name__ == "__main__":
+    a = scatter()
 
-tmp = bookpass.split('.')
-bookoutpass = tmp[0]+"_out."+tmp[1]
-wb.save(bookoutpass)
-system(bookoutpass)
